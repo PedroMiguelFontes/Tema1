4de43408d3d6131077a6364179728bbd431fa5ad
PedroMiguelFontes <pedromiguelmaia13@gmail.com>
2022-01-19 22:32:38 +0000

    Registro e login ligados

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
-let username=document.getElementById("username").value
-let password=document.getElementById("password").value
-let lUsername=document.getElementById("lUsername").value
-let lPassword=document.getElementById("lPassword").value
+var username=document.getElementById("username").value
+var password=document.getElementById("password").value
+
 
 function submeter() {
-    window.alert("Obrigado pelo feedback"+username)
+    window.alert("Obrigado pelo feedback")
 }
 
-function registro() {
+function regista() {
     window.alert("Registrado")
 }
 
 function login() {
+    let lUsername=document.getElementById("lUsername").value
+    let lPassword=document.getElementById("lPassword").value
     if ((username!=lUsername) || (password!=lPassword)) {
         window.alert("Username e/ou password incorreto")
     }
