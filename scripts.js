34f326cfc24c88cf9861e692dd3b45c15a639fa4
PedroMiguelFontes <pedromiguelmaia13@gmail.com>
2022-01-19 02:55:48 +0000

    Registro

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let username=document.getElementById("username")
+let password=document.getElementById("password")
+
+function submeter() {
+    window.alert("Obrigado pelo feedback"+username)
+}
+
+function registro() {
+    window.alert("Registrado")
+}
