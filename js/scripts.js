4853e1152994737b0a59140f48503302cd4c10a7
PedroMiguelFontes <pedromiguelmaia13@gmail.com>
2022-01-21 05:19:43 +0000

    Desnvolvimento da página eltrodomesticos

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
-var username=document.getElementById("username").value
-var password=document.getElementById("password").value
-
-
 function submeter() {
     window.alert("Obrigado pelo feedback")
 }
 
 function regista() {
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    localStorage.setItem("username",username.value);
+    localStorage.setItem("username",password.value);
     window.alert("Registrado")
 }
 
 function login() {
+    var storedUsername=localStorage.getItem("username");
+    var storedPassword=localStorage.getItem("password");
     let lUsername=document.getElementById("lUsername").value
     let lPassword=document.getElementById("lPassword").value
-    if ((username!=lUsername) || (password!=lPassword)) {
+    if ((storedUsername!=lUsername) || (storedPassword!=lPassword)) {
         window.alert("Username e/ou password incorreto")
     }
     else {
         window.alert("Login bem sucedido")
     }
+}
+
+function addTablet() {
+    
 }
